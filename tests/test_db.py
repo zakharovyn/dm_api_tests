58b41616db4ae095d0bfd75e30c94499b04a7903
Zakharov Yan <banjumingray@bk.ru>
2023-11-21 22:57:59 +0700

    ORM

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,5 +1,7 @@
-from generic.helpers.dm_db import DmDatabase
+from generic.helpers.orm_db import OrmDatabase
+from generic.helpers.orm_models import User
 import structlog
+
 
 structlog.configure(
     processors=[
@@ -8,8 +10,25 @@ structlog.configure(
 )
 
 
-def test_db():
+def test_orm():
+    user = 'postgres'
+    password = 'admin'
+    host = '5.63.153.31'
+    database = 'dm3.5'
 
-    db = DmDatabase(user='postgres', password='admin', host='5.63.153.31', database='dm3.5')
-    # db.get_all_users()
-    db.update_activation_status(login='new_user106', activation_status='false')
+    orm = OrmDatabase(user=user, password=password, host=host, database=database)
+    # dataset = orm.get_all_users()
+
+    dataset = orm.update_activation_status(login='new_user106', activation_status=False)
+    dataset = orm.get_user_by_login(login='new_user106')
+    row: User
+    for row in dataset:
+        print(row.Activated)
+        print(row.Login)
+        print(row.Email)
+
+
+
+    print(dataset)
+    orm.db.close_connection()
+
--- tests/test_put_v1_account_password.py
+++ tests/test_put_v1_account_password.py
@@ -1,5 +1,6 @@
 from dm_api_account.models.user_envelope_model import UserRole, Rating
 from hamcrest import assert_that, has_properties
+from generic.helpers.orm_db import OrmDatabase
 from services.dm_api_account import Facade
 import structlog
 
@@ -13,6 +14,7 @@ structlog.configure(
 
 def test_put_v1_account_password():
 
+    orm = OrmDatabase(user='postgres', password='admin', host='5.63.153.31', database='dm3.5')
     api = Facade(host='http://5.63.153.31:5051')
 
     num = '97'
@@ -21,13 +23,27 @@ def test_put_v1_account_password():
     email = f"new_user{num}@email.com"
     password = f"new_user{num}"
 
+    orm.delete_user_by_login(login=login)
+
+    dataset = orm.get_user_by_login(login=login)
+    assert len(dataset) == 0
+
     api.account.register_new_user(
         login=login,
         email=email,
         password=password
     )
 
-    api.account.activate_registered_user(login=login)
+    dataset = orm.get_user_by_login(login=login)
+    for row in dataset:
+        assert row.Login == login
+        assert row.Activated is False
+
+    orm.update_activation_status(login=login, activation_status=True)
+
+    dataset = orm.get_user_by_login(login=login)
+    for row in dataset:
+        assert row.Activated is True
 
     token = api.login.get_auth_token(
         login=login,
