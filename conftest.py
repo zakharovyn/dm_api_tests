5f0b4b18fe0d5c5f1d4a0401cf39cbc5020b453c
Zakharov Yan <banjumingray@bk.ru>
2023-11-24 22:57:46 +0700

    Configuration file

--- conftest.py
+++ conftest.py
@@ -3,6 +3,8 @@ from generic.helpers.dm_db import DmDatabase
 from generic.helpers.orm_db import OrmDatabase
 from services.dm_api_account import Facade
 from collections import namedtuple
+from pathlib import Path
+from vyper import v
 import structlog
 import pytest
 
@@ -15,23 +17,36 @@ structlog.configure(
 
 @pytest.fixture
 def mailhog():
-    return MailhogApi(host='http://5.63.153.31:5025')
+    return MailhogApi(host=v.get('service.mailhog'))
 
 
 @pytest.fixture
 def facade(mailhog):
-    return Facade(host='http://5.63.153.31:5051', mailhog=mailhog)
+    return Facade(
+        host=v.get('service.dm_api_account'),
+        mailhog=mailhog
+    )
 
 
 @pytest.fixture
 def db():
-    db = DmDatabase(user='postgres', password='admin', host='5.63.153.31', database='dm3.5')
+    db = DmDatabase(
+        user=v.get('database.dm3_5.user'),
+        password=v.get('database.dm3_5.password'),
+        host=v.get('database.dm3_5.host'),
+        database=v.get('database.dm3_5.database')
+    )
     return db
 
 
 @pytest.fixture
 def orm():
-    orm = OrmDatabase(user='postgres', password='admin', host='5.63.153.31', database='dm3.5')
+    orm = OrmDatabase(
+        user=v.get('database.dm3_5.user'),
+        password=v.get('database.dm3_5.password'),
+        host=v.get('database.dm3_5.host'),
+        database=v.get('database.dm3_5.database')
+    )
     yield orm
     orm.db.close_connection()
 
@@ -48,3 +63,27 @@ def prepare_user(mailhog, facade, db):
     facade.mailhog.delete_all_messages()
 
     return User
+
+
+options = (
+    'service.dm_api_account',
+    'service.mailhog',
+    'database.dm3_5.host'
+)
+
+
+@pytest.fixture(autouse=True)
+def set_config(request):
+    config = Path(__file__).parent.joinpath('config')
+    config_name = request.config.getoption('--env')
+    v.set_config_name(config_name)
+    v.add_config_path(config)
+    v.read_in_config()
+    for option in options:
+        v.set(option, request.config.getoption(f'--{option}'))
+
+
+def pytest_addoption(parser):
+    parser.addoption('--env', action='store', default='stg')
+    for option in options:
+        parser.addoption(f'--{option}', action='store', default=None)
